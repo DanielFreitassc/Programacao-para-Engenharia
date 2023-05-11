45488eed78916b5ddc79ded4d9fd079e3382f031
Daniel Freitas <daielfreitassc@gmail.com>
2023-05-11 17:53:36 -0300

    for

---
+++ for.py
@@ -0,0 +1,3 @@
+for c in range(1, 6):                   # O laço repetirar o numero de vezes que estivar do range.
+    print("Funcionario {}".format(c))   # Aqui sera mostrado na tela as repetições do range 
+print("Funcionarios Cadastrados")       # Aqui mostra uma mensagem com o fim do for.
