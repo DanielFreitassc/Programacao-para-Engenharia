1680fe2a7876de3d9a936fcf192e0a8bbeb10660
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-15 23:52:17 -0300

    Exercício 11°

---
+++ Lista 12 resolução/décimoprimeiro_exercício_lista_de_programação.py
@@ -0,0 +1,4 @@
+#   11 – Em um script Python com duas listas de três elementos com números
+#   inteiros, crie uma nova lista onde cada elemento é a soma dos elementos de
+#   mesma posição nas duas primeiras listas.
+#    Exemplo: Lista1 = [1, 4, 6] Lista2 = [2, 4, 3] Lista_resultante = [3, 8, 9]
