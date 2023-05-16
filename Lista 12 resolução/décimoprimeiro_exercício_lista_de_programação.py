852e7df81f966ef20b921238307675a810d7e5f6
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-15 23:59:23 -0300

    Exerc´´icio 11°

--- Lista 12 resolução/décimoprimeiro_exercício_lista_de_programação.py
+++ Lista 12 resolução/décimoprimeiro_exercício_lista_de_programação.py
@@ -2,3 +2,12 @@
 #   inteiros, crie uma nova lista onde cada elemento é a soma dos elementos de
 #   mesma posição nas duas primeiras listas.
 #    Exemplo: Lista1 = [1, 4, 6] Lista2 = [2, 4, 3] Lista_resultante = [3, 8, 9]
+
+lista1 = [1,4,6]                             #
+lista2 = [2,4,3]                             #
+lista3 = []                                  #
+
+for lista1, lista2 in zip(lista1, lista2):   #
+    soma = lista1 + lista2                   #
+    lista3.append(soma)                      #
+print(lista3)                                #
