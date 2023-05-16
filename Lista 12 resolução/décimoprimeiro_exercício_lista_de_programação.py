790fef6d9affef8fdbd1618e2b9237b3c7bdb0c2
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-16 00:05:38 -0300

    Exercício 11°

--- Lista 12 resolução/décimoprimeiro_exercício_lista_de_programação.py
+++ Lista 12 resolução/décimoprimeiro_exercício_lista_de_programação.py
@@ -3,11 +3,11 @@
 #   mesma posição nas duas primeiras listas.
 #    Exemplo: Lista1 = [1, 4, 6] Lista2 = [2, 4, 3] Lista_resultante = [3, 8, 9]
 
-lista1 = [1,4,6]                             #
-lista2 = [2,4,3]                             #
-lista3 = []                                  #
+lista1 = [1,4,6]                             # Aqui criamos uma lista com trÊs elementos.
+lista2 = [2,4,3]                             # Aqui criamos um lista com três elementos.
+lista3 = []                                  # Aqui criamos uma lista vazia onde cada elemento é a soma dos elementos da primeira lista.
 
-for lista1, lista2 in zip(lista1, lista2):   #
-    soma = lista1 + lista2                   #
-    lista3.append(soma)                      #
-print(lista3)                                #
+for lista1, lista2 in zip(lista1, lista2):   # Laço  lista1 lista2 combina duas ou mais sequência em pares ordenados.
+    soma = lista1 + lista2                   # soma da lista 1 com a lista 2
+    lista3.append(soma)                      # lista 3 adiciona o valor da soma da lista 1 com a lista 2.
+print(lista3)                                # Aqui mostra ao usuário o valor da nova lista 3.
