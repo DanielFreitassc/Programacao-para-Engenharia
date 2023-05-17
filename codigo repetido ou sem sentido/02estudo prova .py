bfcd640c98864a7ec3e0b55dfc8febf2da61f4a8
Daniel Freitas <daielfreitassc@gmail.com>
2023-05-17 17:24:24 -0300

    Lista de exercício

---
+++ codigo repetido ou sem sentido/02estudo prova .py
@@ -0,0 +1,5 @@
+#   2 - Elabore um script em linguagem Python que leia de 10 números
+#   reais, inserindo-os numa lista e ao final, mostre-os na ordem inversa
+
+for i in range(10):
+    numero = float(input("Insira um número: "))
