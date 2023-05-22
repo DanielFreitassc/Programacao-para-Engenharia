6ba0793371c1c118bd31b061d6498afdda4c6006
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-21 23:23:52 -0300

    login

--- Treino/ex16.py
+++ Treino/ex16.py
@@ -1,16 +1,11 @@
-#   14-Elabore um código que leia a temperatura média de cada mês do ano e
-#   guarde numa lista. Com isso, efetue a média anual das temperaturas e mostre
-#   todas acima da média anual, e em que mês elas ocorreram (Ex 1 – Janeiro, 2 –
-#   Fevereiro, etc)
-
-# temperturadomes = []
-# temperaturas = []
-# for i in range(2):
-#     mes = input("Insira o mês: ")
-#     temp = float(input("Insira a temperatura média do mês: "))
-#     temperaturas.append(temp)
-#     soma = sum(temperaturas)
-#     media = soma / len(temperaturas)
-#     temperturadomes.append(media)
-#     if len(temp) > lent(temperaturas):
-#         print(mes,temp)
+for i in range(5):
+    senha = input("Insira uma senha: ")
+    tentativas = 0
+    while senha != "1234":
+        tentativas += i+1
+        print(f"Senha incorreta você tem 5 chances, tentativa número: {i+1}")      
+        if tentativas == 5:
+            print(f"Conta Bloqueda!")
+        break         
+    else:
+        print("Entrou!")
