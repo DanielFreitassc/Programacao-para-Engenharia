4a3cf7cddbb94612b742a41855dd56df0e85294d
Daniel Freitas <danielfreitassc@gmail.com>
2023-05-28 22:51:15 -0300

    Att

---
+++ Treino/ex22.py
@@ -0,0 +1,9 @@
+NumeroUm = float(input("Insira o primeiro número: "))
+NumeroDois = float(input("Insira o segundo número: "))
+
+if NumeroUm > NumeroDois: 
+    print("O primeiro número é maior")
+elif NumeroUm == NumeroDois:
+    print("Os dois números são iguais")
+else:
+    print("O segundo número é maior")
