91fbde571de000377ae6b4b7c5092495a45d21e3
Daniel Freitas <danielfreitassc@gmail.com>
2023-06-01 23:59:54 -0300

    att

---
+++ Treino/ex73.py
@@ -0,0 +1,6 @@
+def somar(a,b):
+    resultado = a * b
+    return resultado
+valor1 = float(input("Insira um valor: "))
+valor2 = float(input("Insira um valor: "))
+resultadosoma = funcaosoma(valor1, valor2)
