6514a64d6dc45b360815986c3c48069964605f67
Daniel Freitas <daielfreitassc@gmail.com>
2023-06-14 18:11:29 -0300

    att

--- Trabalho FInal/sistema para uma padaria.py
+++ Trabalho FInal/sistema para uma padaria.py
@@ -1,14 +1,7 @@
-while True:
-    print("-="*12)
-    print("[1] Cadastrar Produto")
-    print("[2] Alterar Produto")
-    print("[3] Realizar Vendas")
-    print("[4] Relatórios")
-    print("[5] Sair")
-    print("[6] para sair")
-    print("-="*12)
-    user = int(input("Insira um número: "))
-    if user == 6:
-        break; 
-produto = {"pão":{"valor"}}
+from sys import exit
+import json
+import os 
+produtos = {}
+bancodados = "Estoque.json"
 
+try     
---
+++ Treino/ex69.py
@@ -0,0 +1,6 @@
+num1 = float(input("Insira o numero para dividir: "))
+num2 = float(input("Insira o numero para dividir: "))
+def divisao(n1,n2):
+    return n1/n2
+divide = divisao(num1,num2)
+print(divide)
